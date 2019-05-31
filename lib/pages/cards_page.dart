7f0063d6830c99223ab02befb199ad5058463f0f
Percy Pham <hungpm.percy@gmail.com>
2019-05-31 19:55:44 +0700

    clean up and create simple page

---
+++ lib/pages/cards_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CardsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Cards Page'),
+      ),
+    );
+  }
+}
