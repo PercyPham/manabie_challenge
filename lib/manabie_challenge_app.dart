23c7fe85a99f25a606d65a452d3b5c78558d9dcb
Percy Pham <hungpm.percy@gmail.com>
2019-06-01 20:57:23 +0700

    setup card bloc in UI

---
+++ lib/manabie_challenge_app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:manabie_challenge/bloc/bloc.dart';
+
+import 'package:manabie_challenge/pages/loading_page.dart';
+import 'package:manabie_challenge/pages/cards_page.dart';
+
+class ManabieChallengeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final CardBloc _cardBloc = BlocProvider.of<CardBloc>(context);
+
+    return BlocBuilder<CardEvent, CardState>(
+      bloc: _cardBloc,
+      condition: (previousState, currentState) {
+        return previousState.runtimeType != currentState.runtimeType;
+      },
+      builder: (BuildContext context, CardState state) {
+        if (state is CardUninitialized) {
+          return LoadingPage();
+        } else {
+          return CardsPage();
+        }
+      },
+    );
+  }
+}
